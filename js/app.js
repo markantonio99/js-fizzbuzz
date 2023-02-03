391ac388499959f8933c99928f756b6fd7834914
“markantonio99” <markomakkia@gmail.com>
2023-02-03 01:00:15 +0100

    Ultimo salv

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+for (let numero = 1; numero <= 100; numero++){
+
+    
+
+if ( numero % 3 === 0 ){
+
+    console.log( numero, 'Fizz')
+
+} 
+else if ( numero % 5 === 0 ){
+
+    console.log( numero, 'buzz')
+
+} 
+
+
+
+
+
+
+
+
+//if (i  divisibile per 3)
+
+}
